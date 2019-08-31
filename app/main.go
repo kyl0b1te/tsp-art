6629a8d045a6e06265cd17066155ea5466e89205
zhikiri <zhikiri.dev@gmail.com>
2019-08-31 10:52:09 +0300

    finished with command line argument parsing

--- app/main.go
+++ app/main.go
@@ -1,8 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("Hello")
+	if len(os.Args) != 3 {
+		raiseError(errors.New("Some app arguments are missing"))
+	}
+
+	svg, err := getFilePathSVG()
+	raiseError(err)
+
+	tsp, err := getFilePathTSP()
+	raiseError(err)
+
+	_, err = convert(svg, tsp)
+	raiseError(err)
+}
+
+func getFilePathSVG() (string, error) {
+
+	if os.Args[1] == "" {
+		return "", errors.New("Invalid SVG path")
+	}
+	return os.Args[1], nil
+}
+
+func getFilePathTSP() (string, error) {
+
+	if os.Args[2] == "" {
+		return "", errors.New("Invalid TSP path")
+	}
+	return os.Args[2], nil
+}
+
+func raiseError(err error) {
+	if err != nil {
+		fmt.Printf("[ERROR] %s\n", err.Error())
+		os.Exit(1)
+	}
 }
